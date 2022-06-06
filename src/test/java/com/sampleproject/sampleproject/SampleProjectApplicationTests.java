64417f5b7eff74340e2f3b5ce1f01040e405e93b
Hamza Kahloon <hamza.bashir.kahloon@gmail.com>
2022-06-06 22:18:35 +0100

    adding test

--- src/test/java/com/sampleproject/sampleproject/SampleProjectApplicationTests.java
+++ src/test/java/com/sampleproject/sampleproject/SampleProjectApplicationTests.java
@@ -3,11 +3,20 @@ package com.sampleproject.sampleproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SampleProjectApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(true);
     }
 
 }
